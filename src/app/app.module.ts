8c01c4edfb0e44bd3d1231e48ae6002952c934bb
Major McNeil <MMcNeil@psdata.cc>
2024-11-20 15:48:16 -0500

    Create add-wish-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // BrowserModule is needed for the app to run in a browser
 import { FormsModule } from '@angular/forms'; // Import FormsModule for ngModel
 import { AppComponent } from './app.component'; // Import AppComponent
-import { WishListComponent } from './wish-list/wish-list.component'; // Import your custom component
+import { WishListComponent } from './wish-list/wish-list.component';
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component'; // Import your custom component
 
 @NgModule({
   declarations: [
     AppComponent, // Declare the AppComponent here
-    WishListComponent, // Declare WishListComponent here
+    WishListComponent, AddWishFormComponent, // Declare WishListComponent here
   ],
   imports: [
     BrowserModule, // BrowserModule is required for all applications
